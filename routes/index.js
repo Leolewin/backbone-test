92cc72658c41864aec74f128ae07b318cd290557
leolewin <leolewin@live.cn>
2017-02-17 13:44:17 +0800

    update .gitignore

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,17 @@ router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
   res.sendfile('./views/index.html');
 });
+router.get('/muban', function(req, res, next) {
+  // res.render('index', { title: 'Express' });
+  res.sendfile('./views/muban.html');
+});
+router.get('/muban/model', function(req, res, next) {
+  // res.render('index', { title: 'Express' });
+
+  res.json({'title': 'leona', 'title': 'leolewin'});
+});
+router.post('/muban/model',function(req, res, next){
+	  console.log(req.body);
+})
 
 module.exports = router;
